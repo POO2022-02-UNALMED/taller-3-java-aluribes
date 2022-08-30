292ef6506d92603cc55f5453d95f0c8969776073
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2022-08-30 01:27:39 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,39 +1,48 @@
 package taller3.televisores;
 
+
 public class Control {
-	private TV tv;
-	
-	public void enlazar(TV tv) {
-		this.tv=tv;
-		tv.setControl(this);
-	}
-	
-	public TV getTv() {
-		return this.tv;
-	}
-	
-	public void setT(TV tv) {
-		this.tv=tv;
-	}
-	public void turnOn() {
-		tv.turnOn();
-	}
-	public void turnOff() {
-		tv.turnOff();
-	}
-	public void volumenUp() {
-		tv.volumenUp();
-	}
-	public void volumenDown() {
-		tv.volumenDown();
-	}
-	public void canalUp() {
-		tv.canalUp();
-	}
-	public void canalDown() {
-		tv.canalDown();
-	}
-	public void setCanal(int canal) {
-		tv.setCanal(canal);
-	}
+    private TV tv;
+    public int canal1;
+
+    public TV getTv() {
+        return tv;
+    }
+
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
+
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public void canalUp(){
+        tv.canalUp();
+    }
+
+    public void canalDown(){
+        tv.canalDown();
+    }
+
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+
+    public void setCanal(int canal1){
+        tv.setCanal(canal1);
+    }
+
+    public void turnOn() {
+        tv.turnOn();
+    }
+    
+    public void turnOff() {
+        tv.turnOff();
+    }
 }
