588e70a3805bb8414d25f6473e811fedd1bc5add
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2022-08-30 01:27:49 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,87 +1,105 @@
 package taller3.televisores;
 
 public class TV {
-	private Marca marca;
-	private int canal = 1;
-	private int precio = 500;
-	private boolean estado;
-	private int volumen = 1;
-	private Control control;
-	private static int numTV= 0;
-	
-	public TV(Marca marca, boolean estado) {
-		this.marca= marca;
-		this.estado= estado;
-		numTV+=1;
-	}
-	public void setCanal(int canal) {
-		if(this.estado==true && this.canal>=0 && this.canal<= 120) {
-			this.canal=canal;
-		}
-	}
-	public int getCanal() {
-		return this.canal;
-	}
-	public void setPrecio(int precio) {
-		this.precio=precio;
-	}
-	public int getPrecio() {
-		return this.precio;
-	}
-	public void setVolumen(int volumen) {
-		if(this.estado==true && this.volumen>=0 && this.volumen<= 7) {
-			this.volumen=volumen;
-		}
-	}
-	public int getVolumen() {
-		return this.volumen;
-	}
-	public void setMarca(Marca marca) {
-		this.marca=marca;
-	}
-	public Marca getMarca() {
-		return this.marca;
-	}
-	public void setControl(Control control) {
-		this.control=control;
-	}
-	public Control getControl() {
-		return this.control;
-	}
-	public static int getNumTV() {
-		return numTV;
-	}
-	public static void setNumTV(int Num) {
-		numTV=Num;
-	}
-	public void turnOn() {
-		this.estado= true;
-	}
-	public void turnOff() {
-		this.estado=false;
-	}
-	public boolean getEstado() {
-		return this.estado;
-	}
-	public void canalUp() {
-		if(this.estado==true && 0<=this.canal && this.canal< 120) {
-			this.canal++;
-			}
-		}
-	public void canalDown() {
-		if(this.estado==true && 0<this.canal&& this.canal<= 120) {
-			this.canal--;			}
-		}
-	public void volumenUp() {
-		if(this.estado==true && this.volumen>=0 && this.volumen< 7) {
-			this.volumen++;
-			}
-		}
-	
-	public void volumenDown() {
-		if(this.estado==true && this.volumen>0 && this.volumen<= 7) {
-				this.volumen--;
-			}
-		}
-	
+    private Marca marca;
+    private int canal=1;
+    private int precio=500;
+    private boolean estado;
+    private int volumen=1;
+    private Control control;
+    private static int numTV=0;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        TV.numTV++;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        if (canal<=120 && estado && canal>=1){
+            this.canal = canal;
+        }
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        if (volumen<=7 && estado && volumen>=0){
+            this.volumen = volumen;
+        }
+    }
+
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public static int getNumTV() {
+        return numTV;
+    }
+
+    public static void setNumTV(int numTV) {
+        TV.numTV = numTV;
+    }
+
+    public void turnOn() {
+        this.estado=true;
+    }
+    
+    public void turnOff() {
+        this.estado=false;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void canalUp(){
+        if (canal<120 && estado){
+            this.canal+=1;
+        }
+    }
+
+    public void canalDown(){
+        if (canal>1 && estado){
+            this.canal-=1;
+        }
+    }
+
+    public void volumenUp(){
+        if (volumen<7 && estado){
+            this.volumen+=1;
+        }
+    }
+
+    public void volumenDown(){
+        if (volumen>0 && estado){
+            this.volumen-=1;
+        }
+    }    
 }
